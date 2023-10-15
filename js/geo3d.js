c97b093a66ad8301226bf7a7fedcd22824528f1a
Bengabo <ben@bengabo.com>
2023-10-15 19:48:35 +0200

    threejs file improved

--- js/geo3d.js
+++ js/geo3d.js
@@ -3,14 +3,19 @@
  */
 const container = document.getElementById("threejs-container");
 
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(
-  30,
+  75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-camera.position.z = 6;
+camera.position.z = 6.5;
 
 /**
  * Geometry
@@ -27,65 +32,108 @@ const meshPhysicalMat = new THREE.MeshPhysicalMaterial({
   thickness: 0.5,
 });
 const sphere01 = new THREE.Mesh(
-  new THREE.IcosahedronGeometry(1, 4),
+  new THREE.IcosahedronGeometry(2.8, 4),
   meshBasicMat
 );
-const cone = new THREE.Mesh(new THREE.TetrahedronGeometry(0.15, 0), meshBasicMat);
-cone.position.x = 1.255;
+const cone1 = new THREE.Mesh(
+  new THREE.TetrahedronGeometry(0.35, 0),
+  meshBasicMat
+);
+cone1.position.x = 3.5;
 
 const geoGroup = new THREE.Group();
-geoGroup.position.x = 1;
-geoGroup.position.y = 0.4;
-geoGroup.add(sphere01, cone);
+
+// Cones field
+const addCones = () => {
+  const cone = new THREE.Mesh(
+    new THREE.TetrahedronGeometry(0.15, 0),
+    new THREE.MeshBasicMaterial({
+      color: 0xffe72d,
+      wireframe: true,
+    })
+  );
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(7));
+  cone.position.set(x, y, z);
+  geoGroup.add(cone);
+};
+Array(15).fill().forEach(addCones);
+
+// Cubes field
+const addCubes = () => {
+  const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(0.2, 0.2, 0.2),
+    new THREE.MeshBasicMaterial({
+      color: 0xffe72d,
+      wireframe: true,
+    })
+  );
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(7));
+  cube.position.set(x, y, z);
+  geoGroup.add(cube);
+};
+Array(15).fill().forEach(addCubes);
+
+geoGroup.add(cone1);
+geoGroup.position.x = 2;
+geoGroup.position.y = 0.5;
+sphere01.position.x = 2;
+sphere01.position.y = 0.5;
 scene.add(geoGroup);
+scene.add(sphere01);
 
 /**
  * Renderer
  */
-let renderer = new THREE.WebGLRenderer({
+const renderer = new THREE.WebGLRenderer({
   antialias: true,
   alpha: true,
 });
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 container.appendChild(renderer.domElement);
-// renderer.setSize(
-//   document.getElementById("threejs-container").clientWidth,
-//   document.getElementById("threejs-container").clientHeight
-// );
-// document.getElementById("threejs-container").appendChild(renderer.domElement);
 
 window.addEventListener("resize", function () {
-  let width = document.getElementById("threejs-container").clientWidth;
-  let height = document.getElementById("threejs-container").clientHeight;
-  renderer.setSize(width, height);
-  camera.aspect = width / height;
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  camera.aspect = sizes.width / sizes.height;
   camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
-console.log(
-  "Container height: " +
-    document.getElementById("threejs-container").clientHeight
-);
 
-// const clock = new Clock();
-
-// const tick = () => {
-//   const time = clock.getElapsedTime();
-
-//   renderer.render(scene, camera);
-//   controls.update();
-//   requestAnimationFrame(tick);
-// };
-
-// tick();
+/**
+ * Scroll function
+ */
+const moveCamera = () => {};
+const clock = new THREE.Clock();
 
 // Animation loop
 function tick() {
-  requestAnimationFrame(tick);
+  const elapsedTime = clock.getElapsedTime();
+
   geoGroup.rotation.x += 0.001;
   geoGroup.rotation.y += 0.001;
-  cone.rotation.x += 0.01;
-  cone.rotation.y += 0.01;
+  cone1.rotation.x += 0.01;
+  cone1.rotation.y += 0.01;
+
+  geoGroup.children.forEach((cone) => {
+    cone.rotation.x += 0.005;
+    cone.rotation.y += 0.005;
+  });
+  geoGroup.children.forEach((cube) => {
+    cube.rotation.x += 0.005;
+    cube.rotation.y += 0.005;
+  });
+
+  sphere01.rotation.x += 0.001;
+  sphere01.rotation.y += 0.001;
+
   renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
 }
 
 tick();
